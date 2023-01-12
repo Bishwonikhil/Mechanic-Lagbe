1c095f846e3fd9606112fef69af54872717be6c9
Md. Atikul Islam Sumon <87384205+SumonAtikul@users.noreply.github.com>
2023-01-12 21:44:53 +0600

    Add files via upload

---
+++ lib/ui/payment.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:http/http.dart' as http;
+
+
+
+class PaymentController{
+  Map<String, dynamic>? paymentIntentData;
+
+  Future<void> makePayment(
+  {required String amount, required String currency}) async {
+    try {
+      paymentIntentData = await createPaymentIntent(amount, currency);
+      if (paymentIntentData != null) {
+        await Stripe.instance.initPaymentSheet(
+          paymentSheetParameters: SetupPaymentSheetParameters(
+            paymentIntentClientSecret: paymentIntentData!['client_secret'],
+            applePay: true,
+            googlePay: true,
+            testEnv: true,
+            merchantCountryCode: 'US',
+            merchantDisplayName: 'Prospects',
+            customerId: paymentIntentData!['customer'],
+            customerEphemeralKeySecret: paymentIntentData!['ephemeralKey'],
+          ));
+        displayPaymentSheet();
+      }
+    } catch (e, s){
+      print('exception:$e$s');
+    }
+  }
+
+  displayPaymentSheet() async {
+    try {
+      await Stripe.instance.presentPaymentSheet();
+      print('Payment Successful');
+    } on Exception catch (e) {
+      if (e is StripeException) {
+        print("Error from Stripe: ${e.error.localizedMessage}");
+      } else {
+        print("Unforseen error: ${e}");
+      }
+    } catch (e) {
+      print("exception:$e");
+    }
+  }
+
+  createPaymentIntent(String amount, String currency) async {
+    try {
+      Map<String, dynamic> body = {
+        'amount': calculateAmount(amount),
+        'currency': currency,
+        'payment_method_types[]': 'card'
+      };
+      var response = await http.post(
+        Uri.parse("https://api.stripe.com/v1/payment_intents"),
+        body: body,
+        headers: {
+          'Authorization':
+              'Bearer sk_test_51MPNfGHdowuvC8uFdd5X9MHQggdlAGDMt4o65E01BQ5sRyOK306hJLs6gRzkRr67FWxRB6EYBu5A9icLT8a6e2un00K7sKOTzG',
+          'Content-Type': 'application/x-www-form-urlencoded'
+        });
+      return jsonDecode(response.body);
+    } catch (err) {
+      print('err charging user: ${err.toString()}');
+    }
+  }
+
+  calculateAmount(String amount) {
+    final a = (int.parse(amount)) * 100;
+    return a.toString();
+  }
+}
