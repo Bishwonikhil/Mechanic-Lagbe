1ec0c50a80b434dfe7e690b6193098199c5bd657
Md. Atikul Islam Sumon <87384205+SumonAtikul@users.noreply.github.com>
2023-01-13 08:56:26 +0600

    Add files via upload

--- lib/ui/payment.dart
+++ lib/ui/payment.dart
@@ -3,29 +3,29 @@ import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:http/http.dart' as http;
 
 
-
-class PaymentController{
+class PaymentController {
   Map<String, dynamic>? paymentIntentData;
 
   Future<void> makePayment(
-  {required String amount, required String currency}) async {
+      {required String amount, required String currency}) async {
     try {
       paymentIntentData = await createPaymentIntent(amount, currency);
       if (paymentIntentData != null) {
         await Stripe.instance.initPaymentSheet(
-          paymentSheetParameters: SetupPaymentSheetParameters(
-            paymentIntentClientSecret: paymentIntentData!['client_secret'],
-            applePay: true,
-            googlePay: true,
-            testEnv: true,
-            merchantCountryCode: 'US',
-            merchantDisplayName: 'Prospects',
-            customerId: paymentIntentData!['customer'],
-            customerEphemeralKeySecret: paymentIntentData!['ephemeralKey'],
-          ));
+            paymentSheetParameters: SetupPaymentSheetParameters(
+          paymentIntentClientSecret: paymentIntentData!['client_secret'],
+          applePay: const PaymentSheetApplePay(merchantCountryCode: '+92'),
+          googlePay: const PaymentSheetGooglePay(testEnv: true, currencyCode: "US", merchantCountryCode: '+92'),
+          //testEnv: true,
+          //merchantCountryCode: 'US',
+          merchantDisplayName: 'sumon',
+          customerId: paymentIntentData!['customer'],
+          //paymentIntentClientSecret: paymentIntentData!['client_secret'],
+          customerEphemeralKeySecret: paymentIntentData!['ephemeralKey'],
+        ));
         displayPaymentSheet();
       }
-    } catch (e, s){
+    } catch (e, s) {
       print('exception:$e$s');
     }
   }
@@ -53,13 +53,13 @@ class PaymentController{
         'payment_method_types[]': 'card'
       };
       var response = await http.post(
-        Uri.parse("https://api.stripe.com/v1/payment_intents"),
-        body: body,
-        headers: {
-          'Authorization':
-              'Bearer sk_test_51MPNfGHdowuvC8uFdd5X9MHQggdlAGDMt4o65E01BQ5sRyOK306hJLs6gRzkRr67FWxRB6EYBu5A9icLT8a6e2un00K7sKOTzG',
-          'Content-Type': 'application/x-www-form-urlencoded'
-        });
+          Uri.parse("https://api.stripe.com/v1/payment_intents"),
+          body: body,
+          headers: {
+            'Authorization':
+                'Bearer sk_test_51MPNfGHdowuvC8uFdd5X9MHQggdlAGDMt4o65E01BQ5sRyOK306hJLs6gRzkRr67FWxRB6EYBu5A9icLT8a6e2un00K7sKOTzG',
+            'Content-Type': 'application/x-www-form-urlencoded'
+          });
       return jsonDecode(response.body);
     } catch (err) {
       print('err charging user: ${err.toString()}');
